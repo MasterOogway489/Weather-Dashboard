5fa1d3161747afa95fd2f0c4681c7f5b225a9f76
Sam Wilson <sammy011116@gmail.com>
2023-07-21 22:34:42 -0600

    Added comments to javascript to explain functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,18 @@ let fiveDayTitle = document.getElementById("five-day-title");
 let fiveDayForecastCardsEl = document.getElementById("five-day-forecast-cards");
 let today = dayjs();
 
- 
+ // submit event for the search form
 function submitCity(event) {
     event.preventDefault();
     getCityData(searchInputEl.value);
     searchHistory();
     
 }
+// During a tutoring session as well as through my own searching I found out about
+// async and await. I was able to get the code to work using this method and found
+// it to be much cleaner and opted to use that instead of the .then method. I left
+// the .then method commented out below for reference to show I undertand how it
+// works
 
 // searchBtnEl.addEventListener("click", displayCity);
 
@@ -42,6 +47,9 @@ function submitCity(event) {
  
 // }
 
+// function that gets the city data from the api getting the name of the city from the search input
+// and then using that to get the latitude and longitude of the city. Then it calls the displayCity
+// function as well as the oneCallWeather function, thesearch history function, and the fiveDayForecast function
 async function getCityData(cityName) {
     let requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiId}`
     const response = await fetch(requestUrl)
@@ -57,6 +65,8 @@ async function getCityData(cityName) {
            
 }
 
+// function that displays the city name and the current date as well as the weather icon
+// for the current day
 async function displayCity(city, lat, lon) {
     let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiId}`
     const response = await fetch(forecastUrl);
@@ -72,6 +82,8 @@ async function displayCity(city, lat, lon) {
     
 }
 
+// function that gets the current weather data for the city that was searched for
+// and displays the current temperature, wind speed, and humidity
 async function oneCallWeather(lat, lon) {
     let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiId}`
     const response = await fetch(weatherUrl)
@@ -84,6 +96,8 @@ async function oneCallWeather(lat, lon) {
     displayCurrentData(currentTemp, currentWind, currentHumidity)
 }
 
+// function that gets the five day forecast for the city that was searched for
+// and displays the date, weather icon, temperature, wind speed, and humidity
 async function fiveDayForecast(lat, lon) {
     let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiId}`
     const response = await fetch(forecastUrl);
@@ -117,6 +131,7 @@ async function fiveDayForecast(lat, lon) {
 
 }
 
+// function that displays the current temperature, wind speed, and humidity
 function displayCurrentData(currentTemp, currentWind, currentHumidity) {
     let currentTempEl = document.getElementById("current-temp");
     let currentWindEl = document.getElementById("current-wind-speed");
@@ -128,7 +143,8 @@ function displayCurrentData(currentTemp, currentWind, currentHumidity) {
 
 
 
-
+// function that displays the search history and allows the user to click on a city
+// that they have already searched for to get the weather data for that city again
 function searchHistory(city) {
     let cityList = document.getElementById("search-history-list");
     let cityListItem = document.createElement("li");
@@ -147,4 +163,5 @@ function searchHistory(city) {
     }
 }
 
+// event listener for the search button
 formEl.addEventListener("submit", submitCity);
